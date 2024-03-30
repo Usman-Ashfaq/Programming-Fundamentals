752c69387d72922e10ed44766387dec1a00206d1
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2024-03-31 00:32:08 +0500

    Create capitalize_1st_char_of_sentence.cpp.cpp

---
+++ capitalize_1st_char_of_sentence.cpp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string arr;
+    cout << "Please enter a line:\n";
+    getline(cin, arr);
+
+    bool found = true;
+    for (int i = 0; i != arr.length(); i++)
+    {
+        if (found && (arr[i] >= 'a' && arr[i] <= 'z'))
+        {
+            arr[i] = static_cast<char>(static_cast<int>(arr[i]) - 32);
+            found = false;
+        }
+        else if (arr[i] == '.' || arr[i] == '!' || arr[i] == '?')
+        {
+            found = true;
+        }
+    }
+
+    cout << "Capital string:\n" << arr << endl;
+    system("pause");
+    return 0;
+}
