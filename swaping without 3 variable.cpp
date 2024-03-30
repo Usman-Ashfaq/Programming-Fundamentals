bae93f160f6bd331cfb9d76162938c99d3f398d3
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2024-03-31 00:22:01 +0500

    Add files via upload

---
+++ swaping without 3 variable.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	//int count1, count2, count3, count4, count5;
+//
+//	int a, b;
+//	cin >> a >> b;
+//
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//
+//	cout << a << endl << b;
+//
+//	return 0;
+//}
