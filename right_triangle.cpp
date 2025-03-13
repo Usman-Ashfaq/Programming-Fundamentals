1d06384c393b76e0c7972c8dc4c6b2be07ba222b
Usman Ashfaq <156636786+Usman-Ashfaq@users.noreply.github.com>
2025-03-14 00:42:56 +0500

    Update right_triangle.cpp

--- right_triangle.cpp
+++ right_triangle.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main() {
+    //takigninut from user
     int n;
     cout << "Enter the number of rows: ";
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) {//for rows
         for (int j = 1; j <= i; j++) {
             cout << "*";
         }
